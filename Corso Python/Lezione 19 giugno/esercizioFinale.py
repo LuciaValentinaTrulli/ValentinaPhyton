4117af849b028bb0d922da86e5ed736be1eb4a3f
LuciaValentinaTrulli <150947020+LuciaValentinaTrulli@users.noreply.github.com>
2024-06-19 17:41:11 +0200

    versione a fine giornata

--- Corso Python/Lezione 19 giugno/esercizioFinale.py
+++ Corso Python/Lezione 19 giugno/esercizioFinale.py
@@ -13,11 +13,44 @@ if selezione == "SI":
             if selez3.lower() == "si":
                 selez4 = int(input("Scrivi il numero dell'articolo che vuoi acquistare"))
                 lista_acquisti_utente.append(selez4)
+                print(lista_acquisti_utente)
+            else:
                 break
-            
         else:
             break
-            
+    
+    while controllo_ciclo:
+        articolo1 = {
+            "nome" : "tazza",
+            "prezzo" : 20,
+            "quantità" : 5
+        }
+
+        articolo2 = {
+            "nome" : "borsa",
+            "prezzo" : 10,
+            "quantità" : 12
+        }
+        
+        articolo3 = {
+            "nome" : "penna",
+            "prezzo" : 1,
+            "quantità" : 20
+        }
+        articoli_magazzino = [articolo1, articolo2, articolo3]
+        scelta = input("Scegli l'operazione da effettuare sugli articoli: Aggiunta (A), Rimozione (R), Modifica (M)")
+        if scelta == "A":
+            nome_nuovo = input("Inserisci nome articolo:")
+            prezzo_nuovo = input("Inserisci prezzo articolo:")
+            quantità_nuovo = input("Inserisci quantità articolo:")
+            #penso ci voglia un ciclo per modificare l'indice
+            articolo4 = {}
+            articolo4["nome"] = nome_nuovo
+            articolo4["prezzo"] = prezzo_nuovo
+            articolo4["quantità"] = quantità_nuovo
+            #continuare qui
+
+        
 
 else:
     print("Arrivederci")
