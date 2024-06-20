dd601ad754ac6bd3a961cb6b8c37e539e43eaf0e
LuciaValentinaTrulli <150947020+LuciaValentinaTrulli@users.noreply.github.com>
2024-06-20 12:31:04 +0200

    cartella del 20 giugno

---
+++ Corso Python/lezione 20 giugno/esercizioRange.py
@@ -0,0 +1,19 @@
+#chiedo all'utente di inserire un numero
+numero_inserito = int(input("Inserisci un numero: "))
+
+range(numero_inserito, -1, -1)
+
+#stampo ciclando sugli elementi
+for i in range():
+    print(i)
+
+scelta = input("Vuoi ripetere? (Si o no): ")
+if scelta.lower() == "si":
+    numero_inserito = int(input("Inserisci un numero: "))
+
+    range(numero_inserito, -1, -1)
+
+    for i in range():
+        print(i)
+else:
+    print ("Arrivederci")
