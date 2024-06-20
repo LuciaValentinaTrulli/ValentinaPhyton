8b35c7d392ce408d6f02c4698f88325a6fcd89c7
LuciaValentinaTrulli <150947020+LuciaValentinaTrulli@users.noreply.github.com>
2024-06-20 20:44:59 +0200

    aggiunti esercizi 3 e 4

--- Corso Python/lezione 20 giugno/esercizio2.py
+++ Corso Python/lezione 20 giugno/esercizio2.py
@@ -30,4 +30,56 @@ while controllo:
         print("Il numero non è positivo")
 
 
+#punto 3
+#creazione della lista
+lista=[]
+controllo = True
+while controllo:
+    numero_inserito = float(input("Inserisci un numero: "))
+    lista.append(numero_inserito)
 
+    scelta = input("Vuoi inserire un altro numero? (si o no): ")
+    if scelta.lower() == "no":
+        controllo = False
+
+#calcolo e stampa del quadrato
+for n in lista:
+    quadrato = n * n
+    print (quadrato)
+
+
+#punto 4
+#creazione della lista
+lista=[]
+controllo = True
+while controllo:
+    numero_inserito = int(input("Inserisci un numero intero: "))
+    lista.append(numero_inserito)
+
+    scelta = input("Vuoi inserire un altro numero? (si o no): ")
+    if scelta.lower() == "no":
+        controllo = False
+
+#verifico che la lista non sia vuota
+if  len(lista) == 0:
+    print("Lista vuota")
+    
+else:
+    #trovare numero massimo della lista
+    # lista_ordinata = lista.sort()
+    # lunghezza_lista = len(lista)
+    # indice = lunghezza_lista - 1
+    # massimo = lista_ordinata[indice]
+    massimo = max(lista)
+    print("Il numero massimo tra quelli inseriti è: ")
+    print(massimo)
+
+    #contare i numeri presenti nella lista
+    controllo = True
+    contatore = 0
+    while controllo:
+        for n in lista:
+            contatore+=1
+        print ("I numeri inseriti sono in totale: " )
+        print (contatore)
+        controllo = False    
