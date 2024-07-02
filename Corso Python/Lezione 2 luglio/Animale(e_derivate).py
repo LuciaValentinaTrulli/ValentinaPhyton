0be94b36a1687bd7660f54ed39c7d853fe4a1bfb
LuciaValentinaTrulli <150947020+LuciaValentinaTrulli@users.noreply.github.com>
2024-07-02 17:14:54 +0200

    vers 1

---
+++ Corso Python/Lezione 2 luglio/Animale(e_derivate).py
@@ -0,0 +1,35 @@
+class Animale:
+
+    def __init__(self, nome, età):
+        self.nome = nome
+        self.età = età
+    
+    def fai_suono(self):
+        print("verso animale: ")
+
+
+class Leone(Animale):
+
+    def __init__(self, nome, età, criniera):
+        super().__init__(nome, età)
+        self.criniera = criniera
+       
+
+
+    def fai_suono(self):
+        self.fai_suono()
+        print("verso animale: roar")
+
+
+leone1 = Leone("Simba", 10, "si")
+print(leone1.nome, leone1.età, leone1.criniera)
+leone1.fai_suono()
+
+
+
+
+
+
+
+
+
