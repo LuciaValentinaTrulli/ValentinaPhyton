23afb23746ef95bf3c35a362839e1a81e742f19a
LuciaValentinaTrulli <150947020+LuciaValentinaTrulli@users.noreply.github.com>
2024-07-03 10:39:05 +0200

    es animali funzionante

--- Corso Python/Lezione 2 luglio/Animale(e_derivate).py
+++ Corso Python/Lezione 2 luglio/Animale(e_derivate).py
@@ -1,29 +1,29 @@
-class Animale:
+class Animale:                                          #definizione classe base
 
-    def __init__(self, nome, età):
+    def __init__(self, nome, età):                      #costruttore della classe base
         self.nome = nome
         self.età = età
     
-    def fai_suono():
-        print("verso animale: ")
+    def fai_suono(self):                                #metodo classe base
+        print("verso animale")
 
 
-class Leone(Animale):
+class Leone(Animale):                                   #definizione classe derivata
 
-    def __init__(self, nome, età, criniera):
+    def __init__(self, nome, età, sesso):               #costruttore della classe derivata
         super().__init__(nome, età)
-        self.criniera = criniera
+        self.sesso = sesso
        
+    def fai_suono(self):                                #sovrascrittura del metodo della classe base
+        print("roar")
+    
+    def caccia(self):                                           #metodo della classe derivata
+        print(f"Il leone {self.nome} sta andando a caccia")
 
 
-    def fai_suono():
-        super().fai_suono()
-        print("verso animale: roar")
-
-
-leone1 = Leone("Simba", 10, "si")
-print(leone1.nome, leone1.età, leone1.criniera)
-leone1.fai_suono()
+leone1 = Leone("Simba", 10, "maschio")                  #creo oggetto della classe leone
+leone1.fai_suono()                                      #chiamo metodi della classe leone
+leone1.caccia()
 
 
 
