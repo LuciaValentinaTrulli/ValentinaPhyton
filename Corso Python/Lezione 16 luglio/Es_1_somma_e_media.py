92d58ec15dde1edb88c4cc63b1cb29683e69042b
LuciaValentinaTrulli <150947020+LuciaValentinaTrulli@users.noreply.github.com>
2024-07-16 15:31:49 +0200

    funzionante

---
+++ Corso Python/Lezione 16 luglio/Es_1_somma_e_media.py
@@ -0,0 +1,31 @@
+""" Creare un array NumPy di 15 elementi contenente numeri casuali compresi tra 1 e 100.
+
+Calcolare e stampare la somma di tutti gli elementi dell'array.
+
+Calcolare e stampare la media di tutti gli elementi dell'array. """
+
+import numpy as np
+
+def crea_arr_casuale():
+    #Creo un array di 15 numeri casuali compresi tra 1 e 100
+    arr_random = np.random.randint(1, 100, 15)
+    print("Array di 15 elementi contenente numeri casuali compresi tra 1 e 100:")
+    print(arr_random)
+    return arr_random
+
+def somma(arr_random):
+    #Calcolo la somma degli elementi dell' array
+    somma = np.sum(arr_random)
+    print("La somma degli elementi dell'array è ", somma)
+
+def media(arr_random):
+    #Calcolo la media degli elementi dell' array
+    media = np.mean(arr_random)
+    print("La media degli elementi dell'array è ", media)
+
+
+
+#riciamo le funzioni
+arr_random =crea_arr_casuale()
+somma(arr_random)
+media(arr_random)
