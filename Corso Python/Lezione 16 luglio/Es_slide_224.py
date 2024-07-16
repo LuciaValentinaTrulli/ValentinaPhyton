cb58d7c21769ea9c688cd6c0ac829af0cd78989b
LuciaValentinaTrulli <150947020+LuciaValentinaTrulli@users.noreply.github.com>
2024-07-16 14:07:15 +0200

    reso ripetibile

--- Corso Python/Lezione 16 luglio/Es_slide_224.py
+++ Corso Python/Lezione 16 luglio/Es_slide_224.py
@@ -13,25 +13,68 @@ per la somma parziale. """
 
 import numpy as np
 
-#Creo un array di 50 numeri equidistanti tra 0 e 10 usando linspace
-arr = np.linspace(0, 10, 50) #numero iniziale, finale, numero di elementi voluti
-print("L'array iniziale é:")
-print(arr) 
+def crea_arr_linspace():
+    #Creo un array di 50 numeri equidistanti tra 0 e 10 usando linspace
+    arr = np.linspace(0, 10, 50) #numero iniziale, finale, numero di elementi voluti
+    print("L'array iniziale é:")
+    print(arr)
+    return arr 
 
-#Creo un array di 50 numeri casuali compresi tra 0 e 1
-arr_random = np.random.random(50)
-print("L'array di 50 numeri casuali tra 0 e 1 è")
-print(arr_random)
+def crea_arr_casuale():
+    #Creo un array di 50 numeri casuali compresi tra 0 e 1
+    arr_random = np.random.random(50)
+    print("L'array di 50 numeri casuali tra 0 e 1 è")
+    print(arr_random)
+    return arr_random
 
-#Sommo i due array elemento per elemento per ottenere un nuovo array
-arr_somma = arr + arr_random
-print ("L'array somma è:", arr_somma)
+def somma_elementi(arr, arr_random):
+    #Sommo i due array elemento per elemento per ottenere un nuovo array
+    arr_somma = arr + arr_random
+    print ("L'array somma è:", arr_somma)
+    return arr_somma
 
-#Calcolo la somma totale degli elementi del nuovo array
-somma_nuovo_arr = np.sum(arr_somma)
-print("La somma degli elementi del nuovo array è ", somma_nuovo_arr)
+def somma(arr_somma):
+    #Calcolo la somma totale degli elementi del nuovo array
+    somma_nuovo_arr = np.sum(arr_somma)
+    print("La somma degli elementi del nuovo array è ", somma_nuovo_arr)
 
-#Calcolo la somma degli elementi del nuovo array che sono maggiori di 5
-somma_maggiori_5 = np.sum(arr_somma[arr_somma>5])
-print ("La somma degli elementi del nuovo array che sono maggiori di 5 é", somma_maggiori_5)
+def somma_maggiori_5(arr_somma):
+    #Calcolo la somma degli elementi del nuovo array che sono maggiori di 5
+    somma_maggiori_5 = np.sum(arr_somma[arr_somma>5])
+    print ("La somma degli elementi del nuovo array che sono maggiori di 5 é", somma_maggiori_5)
 
+
+def menu():
+    print("1. Crea un array di 50 numeri equidistanti tra 0 e 10")
+    print("2. Creo un array di 50 numeri casuali compresi tra 0 e 1")
+    print("3. Sommo i due array elemento per elemento per ottenere un nuovo array")
+    print("4. Calcolo la somma totale degli elementi del nuovo array")
+    print("5. Calcolo la somma degli elementi del nuovo array che sono maggiori di 5")
+    print("0. Esci")
+
+
+while True:
+    menu()
+    scelta=input("Inserisci scelta: ")
+    if scelta=="1":
+        crea_arr_linspace()
+    elif scelta=="2":
+        crea_arr_casuale()
+    elif scelta=="3":
+        arr = crea_arr_linspace()
+        arr_random = crea_arr_casuale()
+        somma_elementi(arr, arr_random)
+    elif scelta=="4":
+        arr = crea_arr_linspace()
+        arr_random = crea_arr_casuale()
+        arr_somma = somma_elementi(arr, arr_random)
+        somma(arr_somma)
+    elif scelta=="5":
+        arr = crea_arr_linspace()
+        arr_random = crea_arr_casuale()
+        arr_somma = somma_elementi(arr, arr_random)
+        somma_maggiori_5(arr_somma)
+    elif scelta=="0":
+        break
+    else:
+        print("Scelta sbagliata")
